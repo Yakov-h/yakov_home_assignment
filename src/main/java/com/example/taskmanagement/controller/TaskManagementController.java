c7be219c6908ba4d89baceb3e2c47169d9527d5e
Yakov <yahapps@gmail.com>
2026-02-03 18:12:13 +0200

    createTask http status code fix

--- src/main/java/com/example/taskmanagement/controller/TaskManagementController.java
+++ src/main/java/com/example/taskmanagement/controller/TaskManagementController.java
@@ -6,6 +6,7 @@ import com.example.taskmanagement.dto.requests.UpdateTaskRequest;
 import com.example.taskmanagement.service.TaskManagementService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +25,7 @@ public class TaskManagementController {
 
     //Create a new task
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public TaskDto createTask(@RequestBody @Valid CreateTaskRequest createTaskRequest) {
         return service.createTask(createTaskRequest);
     }
